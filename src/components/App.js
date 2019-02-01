d0b44e4e245bd15b32d6f455f55fbfad9a75bf7d
UchuNoKen <xen.o.fite@gmail.com>
2019-02-01 15:49:19 -0500

    Added submit and clear methods.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Form, FormControl, Button } from "react-bootstrap";
 import Note from "./Note";
+import { bake_cookie, read_cookie, delete_cookie } from "sfcookies";
+
+const cookie_key = "NOTES";
 
 class App extends Component {
   constructor() {
@@ -12,6 +15,10 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    this.setState({ notes: read_cookie(cookie_key) });
+  }
+
   submit() {
     // const notes = this.state.notes;
 
@@ -21,6 +28,14 @@ class App extends Component {
     notes.push({ text });
 
     this.setState({ notes });
+
+    bake_cookie(cookie_key, this.state.notes);
+  }
+
+  clear() {
+    delete_cookie(cookie_key);
+
+    this.setState({ notes: [] });
   }
 
   // <Form inline='true'>
@@ -41,6 +56,8 @@ class App extends Component {
         {this.state.notes.map((note, index) => {
           return <Note key={index} note={note} />;
         })}
+        <hr />
+        <Button onClick={() => this.clear()}>Clear All</Button>
       </div>
     );
   }
