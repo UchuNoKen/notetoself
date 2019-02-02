e44eb588d60ba99b370eabe18143fedb595fe7a4
UchuNoKen <xen.o.fite@gmail.com>
2019-02-02 12:55:04 -0500

    Added test.

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Note extends Component {
+class Note extends React.Component {
   render() {
     return (
       <div className="note">
--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -4,4 +4,8 @@ import Note from "./Note";
 
 describe("Note", () => {
   let note = mount(<Note note={{ text: "test note" }} />);
+
+  it("renders the note text", () => {
+    expect(note.find("p")).toEqual("test note");
+  });
 });
