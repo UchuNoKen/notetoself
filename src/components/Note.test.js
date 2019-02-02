63f996403858c387520a720c53dfc5fc8ede1020
UchuNoKen <xen.o.fite@gmail.com>
2019-02-02 13:31:07 -0500

    Added assertion.

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -2,10 +2,12 @@ import React from "react";
 import { mount } from "enzyme";
 import Note from "./Note";
 
+const props = { note: { text: "test note" } };
+
 describe("Note", () => {
   let note = mount(<Note note={{ text: "test note" }} />);
 
   it("renders the note text", () => {
-    expect(note.find("p")).toEqual("test note");
+    expect(note.find("p").text()).toEqual("abc");
   });
 });
